730c1a923294ba102c2a0281d46da58906d4251c
junyi5257 <junyi5257@163.com>
2018-01-12 17:57:37 +0800

    update info by git

--- spring-cloud-config-eureka-bus/spring-cloud-config-server/src/main/java/com/neo/ConfigServerApplication.java
+++ spring-cloud-config-eureka-bus/spring-cloud-config-server/src/main/java/com/neo/ConfigServerApplication.java
@@ -6,12 +6,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ *
+ * http://www.cnblogs.com/ityouknow/p/6931958.html
+ * @author goujy
+ */
 @EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
-	}
+    /**
+     *
+     *
+     * curl -X POST http://localhost:8001/bus/refresh
+     *
+     * <p>
+     * 在我们使用Bug消息总线时，希望动态更新,在一个客户端中刷新命令;
+     * 可以使用这样的方式;
+     * 需要使用RabbitMq消息中线;
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
 }
